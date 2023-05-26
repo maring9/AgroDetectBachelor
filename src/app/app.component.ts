5ee132e38803d2252c9373aba97962139573acd5
Kristian <kristikotrla@live.com>
2023-05-26 16:15:31 +0200

    changed styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { AuthenticatorService } from '@aws-amplify/ui-angular';
 import { Amplify } from 'aws-amplify';
 import awsmobile from '../aws-exports';
+import { Component } from '@angular/core';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
